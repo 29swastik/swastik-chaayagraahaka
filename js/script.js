d5a0633a681c90e40872bc6696005e1d61d06855
29swastik <kswastik29@gmail.com>
2020-06-14 13:36:43 +0530

    changed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+// Get the modal
+/*var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}*/
+
+function openModal(path){
+
+document.getElementById("myModal").style.display = "block";
+document.getElementById("img01").src = path;
+
+}
+
+function closeModal() {
+
+ document.getElementById("myModal").style.display = "none"; 
+
+}
+
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
@@ -18,3 +50,5 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     $(event.target).focus();
   });
 });
+
+
